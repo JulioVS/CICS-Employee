1c595cd08a2be6677ed3eabadec6fb8046ab2223
JulioVS <julioer@me.com>
2025-05-20 21:42:39 -0300

    Add looping logic, end my try-out exercise

--- CBL/EVIEWP.cbl
+++ CBL/EVIEWP.cbl
@@ -26,6 +26,15 @@
       ******************************************************************
        01 WS-WORKING-VARS.
           05 WS-CICS-RESPONSE   PIC S9(8) USAGE IS BINARY.
+          05 WS-EXIT-FLAG       PIC X(1)  VALUE 'N'.
+             88 WS-EXIT                   VALUE 'Y'.
+      *
+       01 WS-MAX-INDEX          PIC S9(4) USAGE IS BINARY
+                                          VALUE +16.
+      *
+       01 WS-DISPLAY-MESSAGES.
+          05 WS-PF7-LABEL       PIC X(9)  VALUE 'PF7 Prev '.
+          05 WS-PF8-LABEL       PIC X(9)  VALUE 'PF8 Next '.
       *
        01 WS-DEBUG-AID          PIC X(45) VALUE SPACES.
       *
@@ -54,7 +63,13 @@
       *    >>> DEBUGGING ONLY <<<
            MOVE 'MAIN-LOGIC' TO WS-DEBUG-AID.
            PERFORM 9300-DEBUG-AID.
+
+           IF I-AM-DEBUGGING THEN
+              MOVE +3 TO WS-MAX-INDEX
+           END-IF.
       *    >>> -------------- <<<
+
+           PERFORM 1000-FIRST-INTERACTION.
 
            EXEC CICS GET
                 CONTAINER(APP-LIST-CONTAINER-NAME)
@@ -68,60 +83,24 @@
            WHEN DFHRESP(CONTAINERERR)
       *         >>> DEBUGGING ONLY <<<
                 MOVE 'MAIN-> NO CONTAINER FOUND!' TO WS-DEBUG-AID
-                PERFORM 9300-DEBUG-AID
       *         >>> -------------- <<<
-                PERFORM 9000-RETURN-TO-CICS
            WHEN DFHRESP(NORMAL)
       *         >>> DEBUGGING ONLY <<<
                 MOVE 'MAIN-> ALL GOOD, PROCESS INPUT' TO WS-DEBUG-AID
-                PERFORM 9300-DEBUG-AID
       *         >>> -------------- <<<
-                PERFORM 2000-PROCESS-USER-INPUT
+                SET LST-RECORD-INDEX TO 1
+                PERFORM 2000-PROCESS-USER-INPUT UNTIL WS-EXIT
            WHEN OTHER
       *         >>> DEBUGGING ONLY <<<
                 MOVE 'MAIN-> GET CONTAINER ERROR!' TO WS-DEBUG-AID
-                PERFORM 9300-DEBUG-AID
       *         >>> -------------- <<<
-                PERFORM 9000-RETURN-TO-CICS
-           END-EVALUATE.
-
-           EXEC CICS SEND
-                MAP(APP-VIEW-MAP-NAME)
-                MAPSET(APP-VIEW-MAPSET-NAME)
-                FROM (EDETMO)
-                ERASE
-                RESP(WS-CICS-RESPONSE)
-                END-EXEC.
-
-           EXEC CICS RECEIVE
-                MAP(APP-VIEW-MAP-NAME)
-                MAPSET(APP-VIEW-MAPSET-NAME)
-                INTO (EDETMI)
-                RESP(WS-CICS-RESPONSE)
-                END-EXEC.
-
-           EVALUATE EIBAID
-           WHEN DFHENTER
-                MOVE '2000-> Pressed ENTER' TO WS-DEBUG-AID
-           WHEN DFHPF3
-                MOVE '2000-> Pressed PF3' TO WS-DEBUG-AID
-           WHEN DFHPF7
-                MOVE '2000-> Pressed PF7' TO WS-DEBUG-AID
-           WHEN DFHPF8
-                MOVE '2000-> Pressed PF8' TO WS-DEBUG-AID
-           WHEN DFHPF10
-                MOVE '2000-> Pressed PF10' TO WS-DEBUG-AID
-           WHEN DFHPF12
-                MOVE '2000-> Pressed PF12' TO WS-DEBUG-AID
-           WHEN OTHER
-                MOVE '2000-> Invalid Key!' TO WS-DEBUG-AID
            END-EVALUATE.
 
       *    >>> DEBUGGING ONLY <<<
            PERFORM 9300-DEBUG-AID.
       *    >>> -------------- <<<
 
-           CONTINUE.
+           PERFORM 9000-RETURN-TO-CICS.
 
       *-----------------------------------------------------------------
        START-UP SECTION.
@@ -145,7 +124,17 @@
            PERFORM 9300-DEBUG-AID.
       *    >>> -------------- <<<
 
-           MOVE LST-CURRENT-RECORD(1) TO EMPLOYEE-MASTER-RECORD.
+           MOVE LST-CURRENT-RECORD(LST-RECORD-INDEX)
+              TO EMPLOYEE-MASTER-RECORD.
+
+           PERFORM 2100-POPULATE-MAP.
+           PERFORM 2200-SEND-AND-RECEIVE-MAP.
+
+       2100-POPULATE-MAP.
+      *    >>> DEBUGGING ONLY <<<
+           MOVE '2100-POPULATE-MAP' TO WS-DEBUG-AID.
+           PERFORM 9300-DEBUG-AID.
+      *    >>> -------------- <<<
 
            INITIALIZE EDETMO.
 
@@ -187,9 +176,77 @@
                 MOVE 'Unknown' TO DELDSCO
            END-EVALUATE.
 
-           MOVE 'So far so good!' TO MESSO.
+           EVALUATE LST-USER-CATEGORY
+           WHEN 'STD'
+                MOVE 'I am a Standard Employee' TO MESSO
+           WHEN 'MGR'
+                MOVE 'I am a Manager' TO MESSO
+           WHEN 'ADM'
+                MOVE 'I am an Administrator' TO MESSO
+           WHEN OTHER
+                MOVE 'I do not have a Category!' TO MESSO
+           END-EVALUATE.
 
-           CONTINUE.
+           IF LST-RECORD-INDEX IS GREATER THAN 1 THEN
+              MOVE 'PF7 Prev' TO HLPPF7O
+           END-IF.
+           IF LST-RECORD-INDEX IS LESS THAN WS-MAX-INDEX THEN
+              MOVE 'PF8 Next' TO HLPPF8O
+           END-IF.
+
+       2200-SEND-AND-RECEIVE-MAP.
+      *    >>> DEBUGGING ONLY <<<
+           MOVE '2200-SEND-AND-RECEIVE-MAP' TO WS-DEBUG-AID.
+           PERFORM 9300-DEBUG-AID.
+      *    >>> -------------- <<<
+
+           EXEC CICS SEND
+                MAP(APP-VIEW-MAP-NAME)
+                MAPSET(APP-VIEW-MAPSET-NAME)
+                FROM (EDETMO)
+                ERASE
+                RESP(WS-CICS-RESPONSE)
+                END-EXEC.
+
+           EXEC CICS RECEIVE
+                MAP(APP-VIEW-MAP-NAME)
+                MAPSET(APP-VIEW-MAPSET-NAME)
+                INTO (EDETMI)
+                RESP(WS-CICS-RESPONSE)
+                END-EXEC.
+
+           EVALUATE EIBAID
+           WHEN DFHENTER
+                MOVE '2000-> Pressed ENTER' TO WS-DEBUG-AID
+                IF LST-RECORD-INDEX IS LESS THAN WS-MAX-INDEX
+                   SET LST-RECORD-INDEX UP BY 1
+                END-IF
+           WHEN DFHPF3
+                MOVE '2000-> Pressed PF3' TO WS-DEBUG-AID
+                SET WS-EXIT TO TRUE
+           WHEN DFHPF7
+                MOVE '2000-> Pressed PF7' TO WS-DEBUG-AID
+                IF LST-RECORD-INDEX IS GREATER THAN 1
+                   SET LST-RECORD-INDEX DOWN BY 1
+                END-IF
+           WHEN DFHPF8
+                MOVE '2000-> Pressed PF8' TO WS-DEBUG-AID
+                IF LST-RECORD-INDEX IS LESS THAN WS-MAX-INDEX
+                   SET LST-RECORD-INDEX UP BY 1
+                END-IF
+           WHEN DFHPF10
+                MOVE '2000-> Pressed PF10' TO WS-DEBUG-AID
+                SET WS-EXIT TO TRUE
+           WHEN DFHPF12
+                MOVE '2000-> Pressed PF12' TO WS-DEBUG-AID
+                SET WS-EXIT TO TRUE
+           WHEN OTHER
+                MOVE '2000-> Invalid Key!' TO WS-DEBUG-AID
+           END-EVALUATE.
+
+      *    >>> DEBUGGING ONLY <<<
+           PERFORM 9300-DEBUG-AID.
+      *    >>> -------------- <<<
 
       *-----------------------------------------------------------------
        EXIT-ROUTE SECTION.
